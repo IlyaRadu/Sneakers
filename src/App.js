7852775eec7c1b739883f136e739f93300509638
Ilya Radu <radui2004@gmail.com>
2023-07-01 19:57:09 +0200

    component render

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import Header from "./components/Header";
 import Search from "./components/Search";
 import Drawer from "./components/Drawer";
 
+const arr = [
+  { title: "мужские кросовки Nike", price: 79 },
+  { title: "мужские кросовки Addidas", price: 96 },
+  { title: "мужские кросовки Puma", price: 119 },
+  { title: "мужские кросовки NUIU", price: 82 },
+]
+
 function App() {
   return (
       <div className="wrapper clear">
@@ -12,15 +19,15 @@ function App() {
 
         <div className="content p-40">
         <div className="d-flex align-center mb-40 justify-between">
-          <h1> Все кросовки</h1>
+          <h1>All sneakers</h1>
           <Search />
         </div>
 
         <div className="d-flex">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        {arr.map((obj) => (
+          <Card title={obj.title} price={obj.price}/>
+        ))}
+               
         </div>
         </div>
       </div>
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,17 +1,23 @@
-const Card = () => {
-    return ( 
-        <div className="card">
-            <div className="favorite">
+import styles from "./Card.module.scss";
+
+const Card = (props) => {
+    const onClickBtn = () => {
+      alert("Card clicked")
+    }
+
+    return (
+        <div className={styles.card}>
+            <div className={styles.favorite}>
               <img src="/img/unliked.svg" alt="inliked"/>
             </div>
             <img width={133} height={112} src="/img/sneakers/1.jpg" alt="plus"/>
-            <h5>мужские кросовки Nike </h5>
+            <h5>{props.title}</h5>
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
                 <span>Price: </span>
-                <b>79 eur.</b>
+                <b>{props.price}</b>
               </div>
-              <button className="button">
+              <button className="button"  onClick={onClickBtn}>
                 <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
               </button>
             </div>
